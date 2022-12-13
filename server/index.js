1c143962e452643dd8a67ca0f76f63f30266df94
EmilSatyev <satyev.emil@gmail.com>
2022-12-13 19:23:53 +0500

    first

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const {errorHandler,notFound} = require('./middleware/errorMiddleware')
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
