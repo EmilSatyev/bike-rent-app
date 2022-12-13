0f33476d91f48cad1036c536677b502afa5bb736
EmilSatyev <satyev.emil@gmail.com>
2022-12-13 19:30:24 +0500

    first

--- server/index.js
+++ server/index.js
@@ -3,19 +3,17 @@ const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const {errorHandler,notFound} = require('./middleware/errorMiddleware')
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static(__dirname + "/assets"));
 
-// Middleware
-
 
 app.get("/api/test", (req, res) => {
   res.send("Hello worlwd");
 });
+
+app.listen(port, () => console.log(`Server Running on port ${port}`));
 
 mongoose
   .connect(process.env.MONGO_URI)
