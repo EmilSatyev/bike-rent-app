205250ed8ccc809d0201dc08459349f6e5e247cd
EmilSatyev <satyev.emil@gmail.com>
2022-12-13 19:08:27 +0500

    first

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const logger = require("morgan");
-const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-app.use(logger("dev"));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use("/static", express.static(__dirname + "/assets"));
+
 
 app.get("/api/test", (req, res) => {
-  res.send("tesекке");
+  res.send("Hello world");
 });
 
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server Running on port ${port}`));
-
-module.exports = app;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() =>
+    app.listen(port, () => {
+      console.log(`App listening ${port}`);
+    })
+  )
+  .catch((err) => console.log("DB error", err));
