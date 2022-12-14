cb7b4030f86ba94bb0d60c3d83864805b571e51b
EmilSatyev <satyev.emil@gmail.com>
2022-12-14 12:03:03 +0500

    first

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -5,15 +5,16 @@ const User = require("../models/userModel");
 
 // Получить все заказы
 const getOrders = async (req, res) => {
-  try {
-    const orders = await Order.find().populate("cityId").exec();
-    res.status(200).json(orders);
-  } catch (err) {
-    res.status(500).json({
-      message: "Не удалось заказы",
-    });
-    console.warn(err);
-  }
+  // try {
+  //   const orders = await Order.find().populate("cityId").exec();
+  //   res.status(200).json(orders);
+  // } catch (err) {
+  //   res.status(500).json({
+  //     message: "Не удалось заказы",
+  //   });
+  //   console.warn(err);
+  // }
+  res.status(200).json({ cs: "sd" });
 };
 
 // Создать заказ
