5933bffe4063086580988aa12d1420fa6ef3aad2
EmilSatyev <satyev.emil@gmail.com>
2022-12-14 10:15:17 +0500

    first

--- server/controllers/bikes.js
+++ server/controllers/bikes.js
@@ -1,9 +1,14 @@
+const Bike = require("../models/bike");
 const City = require("../models/сity");
+const BikeType = require("../models/type");
+const Brand = require("../models/brand");
+const Size = require("../models/size");
 
 // Получить города
 const getCities = async (req, res) => {
   try {
     const cities = await City.find();
+
     res.status(200).json(cities);
   } catch (err) {
     res.status(500).json({
@@ -13,7 +18,50 @@ const getCities = async (req, res) => {
   }
 };
 
+// Получить типы
+const getTypes = async (req, res) => {
+  try {
+    const types = await BikeType.find();
+
+    res.status(200).json(types);
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось получить типы велосипедов",
+    });
+    console.warn(err);
+  }
+};
+
+// Получить производителей
+const getBrands = async (req, res) => {
+  try {
+    const brands = await Brand.find();
+
+    res.status(200).json(brands);
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось получить производителей",
+    });
+    console.warn(err);
+  }
+};
+
+// Получить размеры
+const getSizes = async (req, res) => {
+  try {
+    const sizes = await Size.find();
+
+    res.status(200).json(sizes);
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось получить размеры",
+    });
+    console.warn(err);
+  }
+};
+
 
 module.exports = {
   getCities,
+  getBrands, getTypes, getSizes
 };
--- server/routes/bikes.js
+++ server/routes/bikes.js
@@ -2,17 +2,20 @@ const express = require("express");
 const router = express.Router();
 const {
   getCities,
+  getSizes,
+  getTypes,
+  getBrands,
 } = require("../controllers/bikes");
 
 
-/*// получить размеры
+// получить размеры
 router.get("/sizes", getSizes);
 
 // получить типы
 router.get("/types", getTypes);
 
 // получить производителей
-router.get("/brands", getBrands);*/
+router.get("/brands", getBrands);
 
 // получить города
 router.get("/cities", getCities);
